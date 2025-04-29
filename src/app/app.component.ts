19f000c7ddf9661485613c612c76dc002a1a6b14
liangshiyun <986517775@qq.com>
2025-04-29 22:09:49 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'file-process';
+}
