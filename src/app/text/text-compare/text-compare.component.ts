5e9a8d8b0f243dc1cea9414c806adcb5570e6c98
liangshiyun <986517775@qq.com>
2025-05-10 01:13:47 +0800

    layout and text-compare optimization

--- src/app/text/text-compare/text-compare.component.ts
+++ src/app/text/text-compare/text-compare.component.ts
@@ -72,63 +72,52 @@ export class TextCompareComponent {
   compareTexts() {
     this.processed1 = '';
     this.processed2 = '';
-
     const text1Array = Array.from(this.text1);
     const text2Array = Array.from(this.text2);
 
-    for (let i = 0; i < Math.min(text1Array.length, text2Array.length); i++) {
-        //value is equal 当字符相同时
-        if (text1Array[0] === text2Array[0]) {
-            this.processed1 += text1Array[0];
-            this.processed2 += text2Array[0];
-            text1Array.splice(0, 1);
-            text2Array.splice(0, 1);
-            i = -1;
-            continue;
-        }
-        //delIndex 当字符为删除字符时
-        const delIndex = text1Array.indexOf(text2Array[0], 1)
-        if (delIndex != -1) {
-            this.processed1 += `<span style='color: #e74c3c;font-weight: 500;text-decoration: line-through 2px;'>` + text1Array.slice(0, delIndex).join('') + '</span>' + text1Array[delIndex];
-            this.processed2 += text2Array[0];
-            text1Array.splice(0, delIndex + 1);
-            text2Array.splice(0, 1);
-            i = -1;
-            continue;
-        }
-        //increase word 当字符为新增字符时
-        const increIndex = text2Array.indexOf(text1Array[0], 1)
-        if (increIndex != -1) {
-            this.processed1 += text1Array[0];
-            this.processed2 += `<span style='color: #2ecc71;font-weight: 500;text-decoration:underline 2px;'>` + text2Array.slice(0, increIndex).join('') + '</span>' + text2Array[increIndex];
-            text1Array.splice(0, 1);
-            text2Array.splice(0, increIndex + 1);
-            i = -1;
-            continue;
-        }
-        //multi match 当字符为修改字符时
-        let equalIndex = -1;
-        for (let j = 1; j < text1Array.length; j++) {
-            const matchIndex = text2Array.indexOf(text1Array[j], 1);
-            if (matchIndex !== -1) {
-                equalIndex = matchIndex;
-                break;
-            }
-        }
-        if (equalIndex != -1) {
-            this.processed1 += `<span style='color: #f39c12;font-weight: 500;'>` + text1Array.slice(0, equalIndex).join('') + '</span>';
-            this.processed2 += `<span style='color: #f39c12;font-weight: 500;'>` + text2Array.slice(0, equalIndex).join('') + '</span>';
-            text1Array.splice(0, 1);
-            text2Array.splice(0, 1);
-            i = -1;
-        }
+    const wrapSpan = (text: string, style: string) => `<span style="${style}">${text}</span>`;
+    const styles = {
+      delete: 'color: #e74c3c;font-weight: 500;text-decoration: line-through 2px;',
+      add: 'color: #2ecc71;font-weight: 500;text-decoration:underline 2px;',
+      modify: 'color: #f39c12;font-weight: 500;'
+    };
+
+    while (text1Array.length > 0 || text2Array.length > 0) {
+      if (text1Array[0] === text2Array[0]) {
+        this.processed1 += text1Array.shift() || '';
+        this.processed2 += text2Array.shift() || '';
+        continue;
+      }
+
+      const delIndex = text1Array.indexOf(text2Array[0], 1);
+      if (delIndex !== -1) {
+        this.processed1 += wrapSpan(text1Array.splice(0, delIndex).join(''), styles.delete) + text1Array.shift();
+        this.processed2 += text2Array.shift() || '';
+        continue;
+      }
+
+      const addIndex = text2Array.indexOf(text1Array[0], 1);
+      if (addIndex !== -1) {
+        this.processed1 += text1Array.shift() || '';
+        this.processed2 += wrapSpan(text2Array.splice(0, addIndex).join(''), styles.add) + text2Array.shift();
+        continue;
+      }
+
+      const modifyIndex = text1Array.findIndex((_, i) => text2Array.indexOf(text1Array[i], 1) !== -1);
+      if (modifyIndex !== -1) {
+        this.processed1 += wrapSpan(text1Array.splice(0, modifyIndex).join(''), styles.modify);
+        this.processed2 += wrapSpan(text2Array.splice(0, modifyIndex).join(''), styles.modify);
+        continue;
+      }
+
+      if (text1Array.length > 0) {
+        this.processed1 += wrapSpan(text1Array.splice(0).join(''), styles.delete);
+      }
+      if (text2Array.length > 0) {
+        this.processed2 += wrapSpan(text2Array.splice(0).join(''), styles.add);
+      }
     }
-    if(text1Array.length > 0) {
-        this.processed1 += `<span style='color: #e74c3c;font-weight: 500;text-decoration: line-through;'>` + text1Array.join('') + '</span>';
-    }
-    if(text2Array.length > 0) {
-        this.processed2 += `<span style='color: #2ecc71;font-weight: 500;'>` + text2Array.join('') + '</span>';
-    }
+
     this.safeContent1 = this.sanitizeHtml(this.processed1);
     this.safeContent2 = this.sanitizeHtml(this.processed2);
   }
